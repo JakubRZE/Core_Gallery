ba29d9b7385a2452b6acf20f4f7a988fcf9e0665
JakubRZE <jakub.rzm@gmail.com>
2019-06-26 15:11:39 +0200

    RCL identity added

---
+++ CoreGallery/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CoreGallery.DAL;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CoreGallery.Areas.Identity.IdentityHostingStartup))]
+namespace CoreGallery.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDefaultIdentity<IdentityUser>()
+                .AddEntityFrameworkStores<AppDbContext>();
+            });
+        }
+    }
+}
