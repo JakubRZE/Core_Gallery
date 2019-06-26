6c418a5c68e6a8c54ecfc6b831e2dde2784c2a3d
JakubRZE <jakub.rzm@gmail.com>
2019-06-26 18:28:42 +0200

    style update

--- CoreGallery/Controllers/HomeController.cs
+++ CoreGallery/Controllers/HomeController.cs
@@ -6,16 +6,21 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CoreGallery.Models;
 using CoreGallery.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.IO;
 
 namespace CoreGallery.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IPhotoRepository _photoRepository;
+        private readonly IHostingEnvironment _appEnvironment;
 
-        public HomeController(IPhotoRepository photoRepository)
+        public HomeController(IPhotoRepository photoRepository, IHostingEnvironment appEnvironment)
         {
             _photoRepository = photoRepository;
+            _appEnvironment = appEnvironment;
         }
 
         public IActionResult Index()
@@ -30,6 +35,33 @@ namespace CoreGallery.Controllers
 
             return View(homeViewModel);
         }
+
+        //Add photo
+        [HttpPost]
+        public async Task<IActionResult> PhotoUpload(IFormFile file)
+        {
+            //< check >
+            if (file == null || file.Length == 0) return Content("file not selected");
+
+            //< get Path >
+            string path_Root = _appEnvironment.WebRootPath;
+            string path_to_Images = path_Root + "\\User_Files\\Images\\" + file.FileName;
+
+            //< Copy File to Target >
+            using (var stream = new FileStream(path_to_Images, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            //< output >
+            ViewData["FilePath"] = path_to_Images;
+            return RedirectToAction("Index", "Home");
+
+        }
+
+
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
